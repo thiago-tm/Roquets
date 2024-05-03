058abe1b1f6c453bf7da696995fba7c255ebfac0
Thiago Tinfre Marcelino <thiagotinfre@hotmail.com>
2024-05-03 14:24:56 -0300

    update-project

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function toogleMode(){
-  const html = document.documentElement
-  html.classList.toggle('light');
-   //pegar a tag img
-   const img = document.querySelector('#profile img');
-   //pegar a classe light
-   //se tiver no light mode, adicionar a imagem avatar2
-   //se tiver sem light mode, adicionar a imagem avatar
-  if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/Avatar2.png');
-  } else {
-    img.setAttribute('src', './assets/Avatar.png');
-  }
+function toggleMode() {
+    const html = document.documentElement;
+    html.classList.toggle('light');
 
- 
-  //substituir a imagem 
-  //se tiver no light mode, adicionar a imagem avatar2
-  //se tiver sem light mode, adicionar a imagem avatar
+    // Pegar a tag img
+    const img = document.querySelector('#profile img');
 
-
-
-
-  
+    // Alterar a imagem de acordo com o modo de tema
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/Avatar2.png');
+    } else {
+        img.setAttribute('src', './assets/Avatar.png');
+    }
 }
